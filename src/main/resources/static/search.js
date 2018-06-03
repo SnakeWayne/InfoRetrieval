ffc5614e9f974f1fc13ac360db042d34e1a51ebd
213150727 <213150727@seu.edu.cn>
2018-06-03 23:39:04 +0800

    23.38

--- src/main/resources/static/search.js
+++ src/main/resources/static/search.js
@@ -16,7 +16,7 @@ function search() {
 
 function docSearch() {
     var str = $("#search-input").val();
-    var url = "/InfoRetrieval/doc";
+    var url = "/InfoRetrieval/plmSearch";
     var json_data = {"keywords": str};
     $.ajax({
         type:'post',
@@ -28,6 +28,44 @@ function docSearch() {
         }
     });
 
+
+}
+
+function docSimilarity() {
+    var str1 = $("#search-input1").val();
+    var str2 = $("#search-input2").val();
+    var json_data = {"doc1": str1,
+    "doc2": str2};
+    var url  = "/InfoRetrieval/similarity";
+    $.ajax({
+        type:'post',
+        url:url,
+        contentType:'application/json;charset=utf-8',
+        data: JSON.stringify(json_data),
+        success:function (data) {
+            var addf = '<div class="row">\n' +
+                '          <div class="col-lg-6">\n' +
+                '            <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">\n' +
+                '              <div class="features-icons-icon d-flex">\n' +
+                '                <i class="icon-screen-desktop m-auto text-primary"></i>\n' +
+                '              </div>\n' +
+                '              <h3>采用w(i,j)计算文档相似度</h3>\n' +
+                '              <p class="lead mb-0">'+data[0]+'</p>\n' +
+                '            </div>\n' +
+                '          </div>\n' +
+                '          <div class="col-lg-6">\n' +
+                '            <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">\n' +
+                '              <div class="features-icons-icon d-flex">\n' +
+                '                <i class="icon-layers m-auto text-primary"></i>\n' +
+                '              </div>\n' +
+                '              <h3>采用wf计算文档相似度</h3>\n' +
+                '              <p class="lead mb-0">'+data[1]+'</p>\n' +
+                '            </div>\n' +
+                '          </div>' +
+                '</div>'
+            $("#baseline").after(addf);
+        }
+    });
 }
 
 function showLyric(data) {
@@ -56,12 +94,12 @@ function showDocRank(data) {
                 '                <i class="icon-layers m-auto text-primary"></i>\n' +
                 '              </div>'+
                 '              <h3>'+data[j].doc+'</h3>\n' +
-                '              <p class="lead mb-0">'+data[j].rank+'</p>\n' +
+                '              <p class="lead mb-0">'+data[j].lyric+'</p>\n' +
                 '            </div>\n' +
                 '          </div>'
         }
         addf += '</div>';
     }
     addf += '</div>';
-    $("#resultsLine").before();
+    $("#resultsLine").before(addf);
 }
