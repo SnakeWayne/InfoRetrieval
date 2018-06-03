f1b227e57ea9253ce73051b57b699ff637ca0ab5
213150727 <213150727@seu.edu.cn>
2018-06-03 23:37:35 +0800

    增加了歌词

--- src/main/java/com/example/infoRetrieval/pojo/RankResult.java
+++ src/main/java/com/example/infoRetrieval/pojo/RankResult.java
@@ -3,9 +3,20 @@ package com.example.infoRetrieval.pojo;
 public class RankResult {
     String doc;
 
-    public RankResult(Object key, Object value) {
+    public String getLyric() {
+        return lyric;
+    }
+
+    public void setLyric(String lyric) {
+        this.lyric = lyric;
+    }
+
+    String lyric;
+
+    public RankResult(Object key, Object value, Object l) {
         doc = (String) key;
         rank = (double) value;
+        lyric = (String) l;
     }
 
     public String getDoc() {
